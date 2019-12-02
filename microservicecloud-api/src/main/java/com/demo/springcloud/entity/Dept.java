44cd3917d75972fa64c10dbd1149e4d82b66d8e0
Leo <memory140662@gmail.com>
2019-12-02 16:09:36 +0800

    add microservicecloud-api

---
+++ microservicecloud-api/src/main/java/com/demo/springcloud/entity/Dept.java
@@ -0,0 +1,18 @@
+package com.demo.springcloud.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+public class Dept implements Serializable {
+    private Long deptno;
+    private String dname;
+    private String dbSource;
+}
