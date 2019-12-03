44474806916023f186b99a1a48dcd8219b2a1844
Leo <memory140662@gmail.com>
2019-12-03 11:27:18 +0800

    add cloud consumer

---
+++ microservice-cloud-consumer-dept-80/src/main/java/com/demo/springcloud/consumer/controller/DeptController.java
@@ -0,0 +1,37 @@
+package com.demo.springcloud.consumer.controller;
+
+import com.demo.springcloud.entities.Dept;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@SuppressWarnings({"unchecked"})
+public class DeptController {
+
+    private static final String REST_URL_PREFIX = "http://localhost:8001";
+
+    @NonNull private RestTemplate restTemplate;
+
+    @PostMapping("/consumer/dept/add")
+    public boolean add(Dept dept) {
+        return restTemplate.postForObject(REST_URL_PREFIX + "/dept/add", dept, Boolean.class);
+    }
+
+    @GetMapping("/consumer/dept/get/{id}")
+    public Dept get(@PathVariable Long id) {
+        return restTemplate.getForObject(REST_URL_PREFIX + "/dept/get" + id, Dept.class);
+    }
+
+    @GetMapping("/consumer/dept/list")
+    public List<Dept> list() {
+        return restTemplate.getForObject(REST_URL_PREFIX + "/dept/list", List.class);
+    }
+}
