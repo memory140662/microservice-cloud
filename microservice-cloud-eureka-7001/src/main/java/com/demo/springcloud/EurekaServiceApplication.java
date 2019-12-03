f878dc53349f8734eafc5f430aecf32a45662965
guilin <memory140662@gmail.com>
2019-12-04 00:54:17 +0800

    修正 版本錯誤 移除 target

--- microservice-cloud-eureka-7001/src/main/java/com/demo/springcloud/EurekaServiceApplication.java
+++ microservice-cloud-eureka-7001/src/main/java/com/demo/springcloud/EurekaServiceApplication.java
@@ -1,13 +1,14 @@
 package com.demo.springcloud;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-//@EnableEurekaServer
-public class EurekaServerApplication {
-
+public class EurekaServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run(EurekaServiceApplication.class, args);
     }
 }
