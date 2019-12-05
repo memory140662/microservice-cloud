dc64244e153bf8ed674478154ee7fc9641f35aba
Leo <memory140662@gmail.com>
2019-12-05 15:42:12 +0800

    新增 hystrix dashboard

---
+++ microservice-cloud-consumer-hystrix-dashboard/src/main/java/com/demo/springcloud/ConsumerHystrixDashboardApplication.java
@@ -0,0 +1,16 @@
+package com.demo.springcloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class ConsumerHystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerHystrixDashboardApplication.class, args);
+    }
+
+}
