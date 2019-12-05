098c09b7d2827c062e6cabf322d0ab3c7fbdb829
Leo <memory140662@gmail.com>
2019-12-05 14:57:38 +0800

    新增 Hystrix 客戶端 服務降級

--- microservice-cloud-api/src/main/java/com/demo/springcloud/service/DeptClientService.java
+++ microservice-cloud-api/src/main/java/com/demo/springcloud/service/DeptClientService.java
@@ -5,11 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@FeignClient("MICROSERVICECLOUD-DEPT")
+@FeignClient(name = "MICROSERVICECLOUD-DEPT", fallbackFactory = DeptClientServiceFallbackFactory.class)
 public interface DeptClientService {
     @PostMapping("/dept/add")
     boolean add(Dept dept);
